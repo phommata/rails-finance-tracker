1f3659e3c174d6f1963fe2bc6ca3cac16864a42c
Andrew Phommathep <13destinies@gmail.com>
2016-08-04 21:57:40 -0700

    Fix to POST add_friend

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
   get 'search_friends', to: "users#search"
-  get 'add_friend', to: "users#add_friend"
+  post 'add_friend', to: "users#add_friend"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
