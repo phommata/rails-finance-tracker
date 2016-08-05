f8a9436bb8a164a0c1ec9f9f479089cb1b18128b
Andrew Phommathep <13destinies@gmail.com>
2016-08-04 21:32:02 -0700

    Fix devise account update

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -8,7 +8,7 @@ class User::RegistrationsController < Devise::RegistrationsController
     # devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
     # devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
